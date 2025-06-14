cfac16047c88e8d616ed40e0ea6167703114cce8
covjus01 <covjus01@gmail.com>
2025-06-14 16:03:32 -0400

    Boiler for Dashboard Workout Display

---
+++ src/components/Dashboard/DashboardWorkouts.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import WorkoutCard from "./WorkoutCard.vue"
+</script>
+<template>
+  <WorkoutCard workout-title="Workout 1">
+    <p>32min 10 exercises</p>
+  </WorkoutCard>
+</template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,29 +1,12 @@
 <script setup lang="ts">
-import WorkoutCard from "../components/Dashboard/WorkoutCard.vue"
+import DashboardWorkouts from "../components/Dashboard/DashboardWorkouts.vue"
 </script>
 
 <template>
   <h2>
     Start
   </h2>
-  <WorkoutCard workout-title="Workout 1">
-    <p>32min 10 exercises</p>
-  </WorkoutCard>
-  <h2>
-    Stats
-  </h2>
-  <AppCard>
-   Graph 1
-  </AppCard>
-  <h2>
-    Build
-  </h2>
-  <AppCard>
-    Edit existing
-  </AppCard>
-  <AppCard>
-    + Add new
-  </AppCard>
+  <DashboardWorkouts />
 
 </template>
 
