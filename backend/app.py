1f08f0924da543f4a5ee80147690c432e94b1297
covjus01 <covjus01@gmail.com>
2025-06-19 23:03:07 -0400

    Added workout endpoint base

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "<p>Hello, World!</p>"
+
+
+# Workout Endpoint
+@app.get("/workout/<workout_name>")
+def get_workout(workout_name):
+    return  {"workout": workout_name}, 200
+
+
+@app.post("/workout/<workout_name>")
+def post_workout(workout_name):
+    return  {"workout": workout_name}, 200
